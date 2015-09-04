fb12fd218bcb18abded6682e33c6b23185acba3c
kilSage <kalshafei@gmail.com>
2015-09-03 23:21:06 -0400

    Added Person3 Info to AntLab

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,16 +65,16 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("gburdell2");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("Joshua Netter");
-                  toPrint += p4.toString("jnetter6");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+		  Person3 p3 = new Person3("Kyle Al-Shafei");
+		  toPrint += p3.toString("kkas3");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("Joshua Netter");
+          toPrint += p4.toString("jnetter6");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("P5");
+          toPrint += p5.toString("gburdell5");
 
         System.out.println(toPrint);
 
