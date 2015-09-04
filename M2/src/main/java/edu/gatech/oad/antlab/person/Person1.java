a2b1c47744e103fc3c1e90c113761d93c08f7a04
t-haich <tomasalfredo95@hotmail.com>
2015-09-04 00:23:13 -0400

    Actually did Gradle stuff -Tomas

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,7 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  input = input.substring(2, input.size) + input.substring(0,2);
+	  input = input.substring(2, input.length()) + input.substring(0,2);
 	  return input;
 	}
 	
