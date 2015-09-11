0a8b1063a49c770375fd7416b324ff65cdf39a83
vegaprime <fullmetal.alchemists@gmail.com>
2015-09-11 18:04:13 -0400

    Panels linked
    Added Button functionality to Main screen to open new window to Config
    Screen

--- Mule Stuff/M3/src/main/MainController.java
+++ Mule Stuff/M3/src/main/MainController.java
@@ -1,19 +1,32 @@
 package main;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+
+import javax.xml.soap.Node;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    public void greeting(ActionEvent event) {
-        System.out.println("Hello");
+    public void greeting(ActionEvent event) throws Exception {
+
+        Parent root = new FXMLLoader().load(getClass().getResource("/fxml/GameConfigDisplay.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        Stage secondaryStage = new Stage();
+        secondaryStage.setScene(scene);
+        secondaryStage.show();
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+        @Override
+        public void initialize (URL location, ResourceBundle resources){
 
-    }
+        }
 }
--- Mule Stuff/M3/src/main/app.java
+++ Mule Stuff/M3/src/main/app.java
@@ -4,9 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 
