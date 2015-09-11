aa8df0795e10031975e03c8d84f5474e4880fc1b
kilSage <kalshafei@gmail.com>
2015-09-11 05:28:39 -0400

    updated gradle files and git ignore and fixed M2 issues

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -55,48 +55,32 @@ import edu.gatech.oad.antlab.pkg2.*;
         String toPrint =
             ant11.getMessage() + ant12.getMessage() + ant21.getMessage()
           + ant22.getMessage() + ant31.getMessage();
-		  //Person1 replace P1 with your name
-		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("Tomas Henriquez");
-		  toPrint += p1.toString("thenriquez3");
-		  //Person2 replace P2 with your name
-		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("gburdell2");
-		  //Person3 replace P3 with your name
-		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("Kyle Al-Shafei");
-		  toPrint += p3.toString("kkas3");
-          //Person4 replace P4 with your name
-          //and gburdell4 with your gt id
-          Person4 p4 = new Person4("Joshua Netter");
-          toPrint += p4.toString("jnetter6");
-          //Person5 replace P4 with your name
-          //and gburdell5 with your gt id
-<<<<<<< HEAD
-          Person5 p5 = new Person5("David Templeton");
-          toPrint += p5.toString("dtempleton6");
-=======
-          Person5 p5 = new Person5("Courtney Ama Rogers");
-          toPrint += p5.toString("crogers60");
->>>>>>> Courtney's-Branch
-
-        System.out.println(toPrint);
-
-
+      //Person1 replace P1 with your name
+      //and gburdell1 with your gt id
+      Person1 p1 = new Person1("Tomas Henriquez");
+      toPrint += p1.toString("thenriquez3");
+      //Person2 replace P2 with your name
+      //and gburdell with your gt id
+      Person2 p2 = new Person2("David Templeton");
+      toPrint += p2.toString("dtempleton6");
+      //Person3 replace P3 with your name
+      //and gburdell3 with your gt id
+      Person3 p3 = new Person3("Kyle Al-Shafei");
+      toPrint += p3.toString("kkas3");
+      //Person4 replace P4 with your name
+      //and gburdell4 with your gt id
+      Person4 p4 = new Person4("Joshua Netter");
+      toPrint += p4.toString("jnetter6");
+      //Person5 replace P4 with your name
+      //and gburdell5 with your gt id
+      Person5 p5 = new Person5("Courtney Ama Rogers");
+      toPrint += p5.toString("crogers60");
+      System.out.println(toPrint);
     }
-
-
     /**
      * entry point for the program
      */
      public static void main(String[] args) {
-
         new AntLabMain().printOutMessage();
-
      }
-
-
-
-
  }
