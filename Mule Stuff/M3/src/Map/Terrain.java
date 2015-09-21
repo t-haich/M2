101cf592c84c8805973cfad6b4e2985dd39c6ba5
jnetter6 <jnetter6@gatech.edu>
2015-09-21 17:11:38 -0400

    Added some map stuff

---
+++ Mule Stuff/M3/src/Map/Terrain.java
@@ -0,0 +1,5 @@
+public enum Terrain {
+    PLAIN(1, 2, 3),
+    RIVER(0, 4, 2),
+    MOUNTAIN(3, 1, 1)
+}
---
+++ Mule Stuff/M3/src/Map/Tile.java
@@ -0,0 +1,7 @@
+public class Tile {
+    //Player owner;
+    Terrain terrain;
+    Mule mule;
+    boolean isOwned = false;
+    boolean hasMule = false;
+}
