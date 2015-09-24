796384afd167e4da69b33ad1d0eb7521b7e7f99e
kilSage <kalshafei@gmail.com>
2015-09-24 18:53:30 -0400

    Map 2d array of Tiles

--- Mule Stuff/M3/src/Map/Map.java
+++ Mule Stuff/M3/src/Map/Map.java
@@ -1,19 +1,35 @@
-
-
 public class Map {
 
-//    Scene scene;
-//    public Map(Scene scene) {
-//        this.scene = scene;
-//        placeTile(RIVER, 0, 0);
-//        placeTile(RIVER, x, y);
-//        placeTile(RIVER, x, y);
-//        placeTile(RIVER, x, y);
-//        for(int i = 0; i < )
-//    }
-//
-//    public placeTile(Terrain type, double xloc, double yloc) {
-//        Tile tile = new Tile(type, xloc, yloc);
-//
-//    }
+    Tile[][] map;
+
+    public Map() {
+        double wide = 66.67;
+        double tall = 80;
+        this.map = new Tile[5][9];
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 9; col++) {
+                if ((col == 2 && row == 0 ) || (col == 8 && row == 2) || (col == 1 && row == 1)) {
+                    map[row][col] = new Tile(MOUNTAIN1, row * wide, col * tall);
+                } else if ((row == 3 && (col ==  1 || col == 6)) || (row == 5 && (col == 2 || col == 8)) {
+                    map[row][col] = new Tile(MOUNTAIN2, row * wide, col * tall);
+                } else if ((row == 0 && col == 6) || (row == 1 && col == 8) || (row == 2 && col == 0) {
+                    map[row][col] = new Tile(MOUNTAIN3, row * wide, col * tall);
+                } else if (col == 4 && (row == 0 || row == 1 || row == 3 || row == 4) {
+                    map[row][col] = new Tile(RIVER, row * wide, col * tall);
+                } else if (row != 2 && row != 4) {
+                    map[row][col] = new Tile(PLAIN, row * wide, col * tall);
+                }
+            }
+        }
+    }
+
+    public Tile getTile(double x, double y) {
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (map[row][col].getX() == x && map[row][col].getY() == y) {
+                    return map[row][col];
+                }
+            }
+        }
+    }
 }
