09a570d36a2d60d777e2e4ef1e8effb430c4064d
t-haich <tomasalfredo95@hotmail.com>
2015-10-19 15:57:59 -0400

    Useless changes

--- Mule Stuff/M3/src/main/TownDisplayController.java
+++ Mule Stuff/M3/src/main/TownDisplayController.java
@@ -6,18 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import Map.Map;
 import Map.Tile;
 import javafx.scene.canvas.GraphicsContext;
 
@@ -29,16 +22,8 @@ public class TownDisplayController implements Initializable {
     public int storeEnergy = 0;
     public int storeFood = 0;
 
-
-    //public void setPrevStage(Stage stage){
-    //    this.prevStage = stage;
-    //}
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //this.map = new Map();
-        //MapController.townStage = new Stage();
-        //shopStage = new Stage();
 
     }
 
@@ -51,19 +36,15 @@ public class TownDisplayController implements Initializable {
         for (int i = 0; i < MapController.tiles.size(); i++) {
             Tile tile = MapController.tiles.get(i);
             System.out.println(tile.getX());
-            //MapController.repaint(tile);
             g2d.fillRect(tile.getX(), tile.getY(), 67, 80);
         }
-        //MapController.townStage.close();
     }
 
     public void toTownScreen() throws IOException {
         Pane myPane;
         myPane = FXMLLoader.load(getClass().getResource("/fxml/Town.fxml"));
         Scene scene = new Scene(myPane);
-        //stage = new Stage();
         app.primaryStage.setScene(scene);
-        //shopStage.close();
     }
 
 
@@ -72,12 +53,9 @@ public class TownDisplayController implements Initializable {
         myPane = FXMLLoader.load(getClass().getResource("/fxml/Shop.fxml"));
         Scene scene = new Scene(myPane);
         app.primaryStage.setScene(scene);
-        //prevStage = new Stage();
-        //shopStage.setScene(scene);
-        //shopStage.show();
     }
 
-    public void toPub() {       //The formula is: Money Bonus = Round Bonus + random between 0 and Time Bonus
+    public void toPub() {
         MapController.timer.cancel();
         MapController.timer.purge();
         long currTime = System.currentTimeMillis();
--- Mule Stuff/M3/src/main/app.java
+++ Mule Stuff/M3/src/main/app.java
@@ -24,21 +24,6 @@ public class app extends Application {
     static Stage primaryStage;
     public static Map map = new Map();
 
-    private Canvas canvas;
-    public static List<Map.Tile> tiles = new ArrayList<Map.Tile>();
-    //public static Map map = new Map();
-    public static Player currPlayer;
-    public static Timer timer;
-    private static boolean tileClicked;
-    public static int turns;
-    public static long turnTime;
-    public static Stage townStage = new Stage();
-    public static int phase = 1;
-    public static int pass = 0;
-    public static int round = 0;
-    public static Player[] arr;
-    public GraphicsContext g2d;
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -59,67 +44,4 @@ public class app extends Application {
         primaryStage.setScene(myScene);
         primaryStage.show();
     }
-
-    public void endTurn() {
-        if (phase1()) {
-            tileClicked = true;
-            pass++;
-            //round++;
-            if (pass == 4) {
-                endPhase();
-                turns(currPlayer);
-            }
-            currPlayer = nextPlayer();
-        }
-    }
-
-    public static Player nextPlayer() {
-        round++;
-        for (int i = 0; i < arr.length; i++) {
-            if (currPlayer.equals(arr[i])) {
-                return arr[(i+1)%arr.length];
-            }
-        }
-        return null;
-    }
-    public boolean phase1() {
-        return phase == 1;
-    }
-
-    public boolean phase2() {
-        return phase == 2;
-    }
-
-    public void endPhase() {
-        if (phase == 1) {
-            phase++;
-        } else if (phase == 2) {
-            phase = 1;
-        }
-    }
-
-    public static void turns(Player p) {
-        int totalTime = PlayerConfigController.getAllTime(round);
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                turnTime = System.currentTimeMillis();
-                turns++;
-                if (turns >= 10) {
-                    phase = 1;
-                    timer.cancel();
-                    timer.purge();
-                } else {
-                    tileClicked = false;
-                    currPlayer.endTurnProduction();
-                    currPlayer = nextPlayer();
-                    System.out.println(currPlayer.toString());
-                    System.out.println("CurrPlayer: " + currPlayer.getMoney());
-                }
-            }
-        }, 0, totalTime);
-    }
-}
-
 }
