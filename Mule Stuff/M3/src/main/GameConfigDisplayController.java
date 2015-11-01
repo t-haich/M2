52aac667d70c36db4a4d1ba54333c1baa55be2cc
Courtney Ama <crogers60@gatech.edu>
2015-11-01 18:31:40 -0500

    Cleaned GameConfig

--- Mule Stuff/M3/src/main/GameConfigDisplayController.java
+++ Mule Stuff/M3/src/main/GameConfigDisplayController.java
@@ -4,22 +4,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameConfigDisplayController implements Initializable {
-
-    Stage prevStage;
-
-    /**
-     * Sets previous screen
-     * @param stage screen prior to this one
-     */
-    public void setPrevStage(Stage stage){
-        this.prevStage = stage;
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -29,7 +18,7 @@ public class GameConfigDisplayController implements Initializable {
      * Displays player screen
      * @throws IOException just in case there is no input/it failed
      */
-    public void toPlayerScreen() throws IOException {
+    public final void toPlayerScreen() throws IOException {
         Pane myPane;
         myPane = FXMLLoader.load(getClass().getResource("/fxml/PlayerConfig.fxml"));
         Scene scene = new Scene(myPane);
