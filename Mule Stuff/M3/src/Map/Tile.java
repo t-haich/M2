7b18f6c22553a35a6b23b390183dbcc2ce330262
vegaprime <fullmetal.alchemists@gmail.com>
2015-11-01 18:46:32 -0500

    PMD, CheckStyle for Tile class

--- Mule Stuff/M3/src/Map/Tile.java
+++ Mule Stuff/M3/src/Map/Tile.java
@@ -1,4 +1,5 @@
-package Map;
+package Map ;
+
 import Characters.*;
 import main.app;
 
@@ -10,12 +11,15 @@ public class Tile {
     private double x;
     private double y;
     private Player owner;
+    private final int tileOffSet = 68;
+    private final double tileMulti = 1.2;
 
     /**
-     * The constructor for the tile
+     * The constructor for the tile.
+     *
      * @param type The terrain type
-     * @param x The x-coordinate of the tile
-     * @param y The y-coordinate of the tile
+     * @param x    The x-coordinate of the tile
+     * @param y    The y-coordinate of the tile
      */
     public Tile(Terrain type, double x, double y) {
         this.type = type;
@@ -26,42 +30,47 @@ public class Tile {
     }
 
     /**
-     * Gets the x-coordinate of the tile
+     * Gets the x-coordinate of the tile.
+     *
      * @return the x-coordinate
      */
-    public double getX() {
-         return x;
+    public final double getX() {
+        return x;
     }
 
     /**
-     * Gets the y-coordinate of the tile
+     * Gets the y-coordinate of the tile.
+     *
      * @return the y-coordinate
      */
-    public double getY() {
-         return y;
+    public final double getY() {
+        return y;
     }
 
     /**
      * Checks if the tile is owned
+     *
      * @return Whether or not the tile is owned
      */
-    public boolean isOwned() {
+    public final boolean isOwned() {
         return owner != null;
     }
 
     /**
      * Checks whether or not a mule is at the tile
+     *
      * @return Whether or not a mule is at the tile via true or false
      */
-    public boolean hasMule() {
-       return hasMule;
+    public final boolean hasMule() {
+        return hasMule;
     }
 
     /**
      * The setter method that officially gives the tile a mule
+     *
      * @param m The desired mule to be placed
      */
-    public void setMule(Mule m) {
+    public final void setMule(Mule m) {
         mule = m;
         hasMule = true;
     }
@@ -69,37 +78,40 @@ public class Tile {
     /**
      * Removes the mule from a tile
      */
-    public void removeMule() {
+    public final void removeMule() {
         mule = null;
         hasMule = false;
     }
 
     /**
      * Sets the owner
+     *
      * @param p The desired owner
      */
-    public void setOwner(Player p) {
+    public final void setOwner(Player p) {
         owner = p;
     }
 
     /**
      * The getter method to access who owns the tile
+     *
      * @return The owner
      */
-    public Player getOwner() {
+    public final Player getOwner() {
         return owner;
     }
 
     /**
      * The getter method that accesses the mule at the tile
+     *
      * @return Returns the mule at the desired tile
      */
-    public Mule getMule() {
+    public final Mule getMule() {
         return mule;
     }
 
     @Override
-    public String toString() {
+    public final String toString() {
         String out = "";
         out += "( " + x + ", " + y + ", " + type.label() + ")";
         return out;
@@ -107,16 +119,17 @@ public class Tile {
 
     /**
      * Gets the amount of neighboring tiles that are owned
+     *
      * @return Returns the number of adjacent, owned tiles
      */
-    public int numAdjacentTiles() {
+    public final int numAdjacentTiles() {
         int total = 0;
         Tile tempTile = app.map.getTile((this.x - 1), this.y);
         if (tempTile != null && tempTile.getOwner() != null && tempTile.getOwner().equals(this.getOwner())
                 && tempTile.hasMule() && tempTile.getMule().outfit().equals(this.getMule().outfit())) {
             total++;
         }
-        tempTile = app.map.getTile((this.x + 68), this.y);
+        tempTile = app.map.getTile((this.x + tileOffSet), this.y);
         if (tempTile != null && tempTile.getOwner() != null && tempTile.getOwner().equals(this.getOwner())
                 && tempTile.hasMule() && tempTile.getMule().outfit().equals(this.getMule().outfit())) {
             total++;
@@ -126,7 +139,7 @@ public class Tile {
                 && tempTile.hasMule() && tempTile.getMule().outfit().equals(this.getMule().outfit())) {
             total++;
         }
-        tempTile = app.map.getTile(this.x, (this.y + 68));
+        tempTile = app.map.getTile(this.x, (this.y + tileOffSet));
         if (tempTile != null && tempTile.getOwner() != null && tempTile.getOwner().equals(this.getOwner())
                 && tempTile.hasMule() && tempTile.getMule().outfit().equals(this.getMule().outfit())) {
             total++;
@@ -136,25 +149,26 @@ public class Tile {
 
     /**
      * Calculates the production of mule at the tile
+     *
      * @param p The player whose production will be calculated
      */
-    public void getProduction(Player p) {
+    public final void getProduction(Player p) {
         double mult = 1;
         if (this.getMule().outfit().equals("Energy")) {
             for (int i = 0; i < this.numAdjacentTiles(); i++) {
-                mult = mult * 1.2;
+                mult = mult * tileMulti;
             }
             p.addEnergy((int) Math.ceil(this.type.energy() * mult));
         }
         if (this.getMule().outfit().equals("Miner")) {
             for (int i = 0; i < this.numAdjacentTiles(); i++) {
-                mult = mult * 1.2;
+                mult = mult * tileMulti;
             }
             p.addSmithore((int) Math.ceil(this.type.smithore() * mult));
         }
         if (this.getMule().outfit().equals("Farmer")) {
             for (int i = 0; i < this.numAdjacentTiles(); i++) {
-                mult = mult * 1.2;
+                mult = mult * tileMulti;
             }
             p.addFood((int) Math.ceil(this.type.food() * mult));
         }
